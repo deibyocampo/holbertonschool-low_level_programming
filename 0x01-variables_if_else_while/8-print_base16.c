d9d2a18243ee35bbe8cc4c68f25b3e9f7ffc5d78
deibyocampo <deiby13_96@hotmail.com>
2019-06-21 22:18:08 +0000

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ **/
+int main(void)
+{
+char n;
+for (n = '0'; n <= '9'; n++)
+putchar(n);
+for (n = 97; n <= 102; n++)
+putchar(n);
+putchar('\n');
+return (0);
+}
