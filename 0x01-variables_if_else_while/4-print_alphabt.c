ddcab3772e21d500623c45d401deae7b98b1d038
deibyocampo <deiby13_96@hotmail.com>
2019-06-21 02:53:54 +0000

    alphabet soup

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ **/
+int main(void)
+{
+char s;
+for (s = 'a'; s <= 'z'; s++)
+{
+if (s != 'e' && s != 'q')
+{
+putchar(s);
+}
+}
+putchar('\n');
+return (0);
+}
