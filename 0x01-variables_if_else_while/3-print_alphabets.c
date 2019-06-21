87d12aeb0b3f49ed4b3a76a87ecf7ffa1c2aecb4
deibyocampo <deiby13_96@hotmail.com>
2019-06-21 01:54:04 +0000

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ **/
+int main(void)
+{
+char s;
+for (s = 'a'; s <= 'z'; s++)
+putchar(s);
+for (s = 'A'; s <= 'Z'; s++)
+putchar(s);
+putchar('\n');
+return (0);
+}
