ffd89404da4ac6b597a31facad659da936fd6db2
deibyocampo <deiby13_96@hotmail.com>
2019-06-21 01:30:41 +0000

    I sometimes suffer from insomnia

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ **/
+int main(void)
+{
+char s;
+for (s = 'a'; s <= 'z'; s++)
+putchar(s);
+putchar('\n');
+return (0);
+}
