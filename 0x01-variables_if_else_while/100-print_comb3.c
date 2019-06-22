700f644354982b07a914544441fb8e600c8c8760
deibyocampo <deiby13_96@hotmail.com>
2019-06-22 15:45:45 +0000

    Inventing is a combination

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ **/
+int main(void)
+{
+int ab;
+int bc;
+for (ab = 48 ; ab <= 56 ; ab++)
+{
+for (bc = ab + 1 ; bc <= 57 ; bc++)
+{
+putchar(ab);
+putchar(bc);
+if (ab == 56 && bc == 57)
+{
+continue;
+}
+else
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+return (0);
+}
