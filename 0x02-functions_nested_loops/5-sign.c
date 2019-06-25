4ebd794bcab7847f4b70fe87e73e7404695a5513
deibyocampo <deiby13_96@hotmail.com>
2019-06-25 23:43:28 +0000

    sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,28 +1,27 @@
 #include "holberton.h"
 /**
- * print_sing - check the code for Holberton School students.
+ * print_sign - verify if the variable is larger or smaller print it in + or.
  *
  * @n: the caracter to print.
  *
- * Return: will return 1 if the variable is greater than 0 will return 0 if the variable equals 0.
+ * Return: will return 1 if the variable is greater than 0 will
+ * return 0 if the variable equals 0.
  */
 int print_sign(int n)
 {
-  if (n > 0)
-    {
-      _putchar('+');
-      return (1);
-      
-    }
-  else if (n == 0)
-    {
-     
-      _putchar('0');
-      return (0);
-    }
-  else
-    {      
-      _putchar('-');
-      return(-1);
-    }
+if (n > 0)
+{
+_putchar('+');
+return (1);
+}
+else if (n == 0)
+{
+_putchar('0');
+return (0);
+}
+else
+{
+_putchar('-');
+return (-1);
+}
 }
