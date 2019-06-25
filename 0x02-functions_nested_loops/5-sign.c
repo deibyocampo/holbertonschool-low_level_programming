4779ab33e8cbb3af88e6035f355ad364957a2062
deibyocampo <deiby13_96@hotmail.com>
2019-06-25 23:26:51 +0000

    sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,13 +2,27 @@
 /**
  * print_sing - check the code for Holberton School students.
  *
- * Return: Always 0.
+ * @n: the caracter to print.
+ *
+ * Return: will return 1 if the variable is greater than 0 will return 0 if the variable equals 0.
  */
 int print_sign(int n)
 {
-  if (n > '0')
+  if (n > 0)
     {
+      _putchar('+');
       return (1);
-      _putchar(43);
+      
     }
-  else if (
+  else if (n == 0)
+    {
+     
+      _putchar('0');
+      return (0);
+    }
+  else
+    {      
+      _putchar('-');
+      return(-1);
+    }
+}
