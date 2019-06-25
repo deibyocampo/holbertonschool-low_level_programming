8514209aa8a2f556b93876ab51b56b27c7bc9225
deibyocampo <deiby13_96@hotmail.com>
2019-06-25 03:53:44 +0000

    loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ **/
+int main (void)
+{
+  int letras;
+  for (letras = 'a'; letras <= 'z'; letras++)
+    {
+      _putchar(letras);
+    }
+  _putchar(letras);
+return (0);
+}
