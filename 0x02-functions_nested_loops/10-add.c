16c3b95640b236a9446c7722fc0c88f573b4ff88
deibyocampo <deiby13_96@hotmail.com>
2019-06-26 22:24:01 +0000

    result

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ * add - print result of multiplication of two numbers.
+ * @a: integer
+ * @b: integer
+ * Return: the valor this is result.
+ */
+int add(int a, int b)
+{
+return (a + b);
+}
