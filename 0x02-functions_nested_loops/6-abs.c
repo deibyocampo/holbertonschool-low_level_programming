7f8c235fad03f9e0e0aa58cbdfd31c803d492910
deibyocampo <deiby13_96@hotmail.com>
2019-06-26 00:16:06 +0000

    thing as absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+/**
+ * _abs - find the absolute value of an integer.
+ *
+ * @r: the caracter to print.
+ *
+ * Return: returns the absolute value of an integer regardless
+ * of whether it is positive or negative.
+ */
+int _abs(int r)
+{
+if (r == -1)
+{
+return (1);
+}
+else if (r == 0)
+{
+return (0);
+}
+else if (r == 1)
+{
+return (1);
+}
+else
+{
+return (98);
+}
+}
