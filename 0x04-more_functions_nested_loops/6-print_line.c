385dd776cfde685d49789142a94a6e274018f7cc
deibyocampo <deiby13_96@hotmail.com>
2019-06-27 22:57:59 +0000

    I feel like

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * print_line - 
+ *
+ * Return: 
+ */
+void print_line(int n)
+{
+  int m;
+  if (n > 0)
+    {
+  for (m = 0; m < n; m++)
+    {
+      _putchar('_');
+    }
+  
+    }
+_putchar('\n');
+}
--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -6,5 +6,6 @@ int mul(int a, int b);
 void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
+void print_line(int n);
 int _putchar(char c);
 #endif /* _SAMPLE_HEADER_H_ */
