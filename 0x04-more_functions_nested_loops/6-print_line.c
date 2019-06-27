30b68ba639220168029e7373532c1b34e62d7567
deibyocampo <deiby13_96@hotmail.com>
2019-06-27 23:07:37 +0000

    returns the variables in _

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,19 +1,20 @@
 #include "holberton.h"
 /**
- * print_line - 
+ * print_line - print _ instead of numbers
  *
- * Return: 
+ * @n: variable that has the values to print _
+ *
+ * Return: returns the variables in _
  */
 void print_line(int n)
 {
-  int m;
-  if (n > 0)
-    {
-  for (m = 0; m < n; m++)
-    {
-      _putchar('_');
-    }
-  
-    }
+int m;
+if (n > 0)
+{
+for (m = 0; m < n; m++)
+{
+_putchar('_');
+}
+}
 _putchar('\n');
 }
