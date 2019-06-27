300168db536b402cc190500e623830969240dd26
deibyocampo <deiby13_96@hotmail.com>
2019-06-27 20:58:10 +0000

    Numbers constitute

--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -5,5 +5,6 @@ int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
 void print_most_numbers(void);
+void more_numbers(void);
 int _putchar(char c);
 #endif /* _SAMPLE_HEADER_H_ */
