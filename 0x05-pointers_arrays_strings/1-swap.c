5d54b57e9e7b1ccaf0b9181caf045f74bf68b712
deibyocampo <deiby13_96@hotmail.com>
2019-07-01 21:50:57 +0000

    swap horses

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ * swap_int - 
+ *
+ *
+ *
+ */
+void swap_int(int *a, int *b)
+{
+  *a = 42;
+  *b = 98;
+}
---
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -0,0 +1,5 @@
+#ifndef _SAMPLE_HEADER_H_
+#define _SAMPLE_HEADER_H_
+void swap_int(int *a, int *b);
+void reset_to_98(int *n);
+#endif /* _SAMPLE_HEADER_H_ */
