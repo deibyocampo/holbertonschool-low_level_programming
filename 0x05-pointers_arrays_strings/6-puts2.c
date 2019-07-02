d22c93205c7ed476fb1a9c08f4be4df23db85616
deibyocampo <deiby13_96@hotmail.com>
2019-07-02 22:37:47 +0000

    Half the lies

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,23 +1,23 @@
 #include "holberton.h"
 /**
- * puts2 -  
+ * puts2 - prints multiples of two.
  *
- * @str: 
+ * @str: character array.
  *
- * return: 
+ * return: prints multiples of two.
  */
 void puts2(char *str)
 {
-  int n;
-  int m;
-  n = 0;
-  m = 0;
-  while (str[n] != '\0')
-      n++;
-  while (m < n)
-    {
-      _putchar(str[m]);
-      m += 2;
-    }
-  _putchar('\n');
+int n;
+int m;
+n = 0;
+m = 0;
+while (str[n] != '\0')
+n++;
+while (m < n)
+{
+_putchar(str[m]);
+m += 2;
+}
+_putchar('\n');
 }
