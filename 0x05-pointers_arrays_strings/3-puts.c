8eca01816067bcc1132c74593d4324b4cbc4a0cd
deibyocampo <deiby13_96@hotmail.com>
2019-07-02 15:01:42 +0000

    fear computers

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,18 +1,18 @@
 #include "holberton.h"
 /**
- *_puts - Recived a string from str, uses _putchar
- * to print every character on the screen. With a new line after
+ * _puts - Recived a string from str, uses _putchar.
+ * to print every character on the screen. With a new line after.
  *
- *@str: The string that will be recived from
+ * @str: The string that will be recived from.
  */
 void _puts(char *str)
 {
-  int m;
-  m = 0;
-  while (str[m] != '\0')
-    {
-      _putchar(str[m]);
-      m++;
-    }
-  _putchar('\n');
+int m;
+m = 0;
+while (str[m] != '\0')
+{
+_putchar(str[m]);
+m++;
+}
+_putchar('\n');
 }
