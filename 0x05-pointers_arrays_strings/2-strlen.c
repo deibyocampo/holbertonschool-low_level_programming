e889b7a6acdf82955d2630db18161416f6828c81
deibyocampo <deiby13_96@hotmail.com>
2019-07-02 14:53:55 +0000

    its very length

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,16 +2,17 @@
 /**
  * _strlen - count every memory space in the arrray.
  *
- * 
+ * @s: variable where it has the string of characters.
  *
+ * Return: prints on screen the length of the variable.
  */
 int _strlen(char *s)
 {
-  int len;
-  len = 0;
-  while (s[len] != '\0')
-    {
-      len++;
-     }
-  return (len);
+int len;
+len = 0;
+while (s[len] != '\0')
+{
+len++;
+}
+return (len);
 }
