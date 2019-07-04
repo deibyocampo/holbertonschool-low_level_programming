af25d13f3777b94ae01fa2d8aea77ce563cbb719
deibyocampo <deiby13_96@hotmail.com>
2019-07-04 18:15:19 +0000

    pointers and arrays

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+ * *_strcat - function to print a string of characters.
+ *
+ * @dest: variable where the character string is stored.
+ *
+ * @src: variable leading the way.
+ *
+ * Return: character string.
+ */
+char *_strcat(char *dest, char *src)
+{
+int agg;
+int comp;
+agg = 0;
+comp = 0;
+while (dest[agg] != '\0')
+agg++;
+while (src[comp] != '\0')
+{
+dest[agg + comp] = src[comp];
+comp++;
+}
+return (dest);
+}
