52df43ffbcbca239f786630ab20f5c265b92483c
deibyocampo <deiby13_96@hotmail.com>
2019-07-04 22:50:08 +0000

    strncpy

--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -1,5 +1,6 @@
 #ifndef _SAMPLE_HEADER_H_
 #define _SAMPLE_HEADER_H_
+char *_strncpy(char *dest, char *src, int n);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 #endif /* _SAMPLE_HEADER_H_ */
