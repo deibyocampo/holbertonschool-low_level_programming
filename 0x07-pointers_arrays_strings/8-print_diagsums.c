ac0af128643bf0e5795c5e43d0efdbce46592218
deibyocampo <deiby13_96@hotmail.com>
2019-07-09 00:16:07 +0000

    Double pointer

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * print_diagsums - prints the sum of the two diagonals of
+ * a square matrix of integers.
+ *
+ * @a: pointer array
+ * @size: size of array
+ *
+ * Return: prints the sum
+ */
+void print_diagsums(int *a, int size)
+{
+	int suma1;
+	int suma2;
+	int b = 0;
+	int c = size - 1;
+
+	while (b < size * size)
+	{
+		suma1 += a[b];
+		suma2 += a[c];
+
+		b += size + 1;
+		c += size - 1;
+	}
+	printf("%d, %d\n", suma1, suma2);
+}
---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ * set_string - sets the value of a pointer to a char.
+ *
+ * @s: pointer doulble.
+ * @to: pointer.
+ *
+ * Return: pointer of a character.
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
