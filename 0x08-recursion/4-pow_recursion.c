fbeaf7ad990909918e74aa1a6beb7e578c3f5183
deibyocampo <deiby13_96@hotmail.com>
2019-07-10 20:45:57 +0000

    pow_recursion

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -17,13 +17,14 @@ int _pow_recursion(int x, int y)
 	{
 		r = 1;
 	}
+
 	if (y > 0)
 	{
 		return (x * _pow_recursion(x, y - 1));
 	}
-	if (y > 0)
+	if (y < 0)
 	{
-		return (r / x * _pow_recursion(x, y + 1));
+		return (-1);
 	}
 	return (r);
 }
