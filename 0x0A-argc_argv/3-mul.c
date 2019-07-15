273e0813d4f85fa93c1e45ef91dab69d76f7d354
deibyocampo <deiby13_96@hotmail.com>
2019-07-15 00:32:11 +0000

    sarcasm is argument

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 /**
  * main - is a function returning an integer.
  *
@@ -11,8 +12,22 @@
 int main(int argc, char *argv[])
 {
 	int i;
-	for (i = 0; i < argc; i++)
-		printf("%s\n", argv[i]);
+	int mul;
+
+	mul = 1;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		mul *= atoi(argv[i]);
+	}
+
+	printf("%d\n", mul);
 
 	return (0);
 }
