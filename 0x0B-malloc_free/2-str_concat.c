14c8df9d7b5409b45053f113861a3d8f6334632b
deibyocampo <deiby13_96@hotmail.com>
2019-07-16 23:45:58 +0000

    courageous enough

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,45 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * *str_concat - concatenates two string.
+ *
+ * @s1: string 1.
+ *
+ * @s2: string 2.
+ *
+ * Return: two string in one.
+ */
+char *str_concat(char *s1, char *s2)
+{
+	int a;
+	int b;
+	int c;
+	char *s3;
+
+	if (s1 == NULL)
+		s1 = " ";
+
+	if (s2 == NULL)
+		s2 = " ";
+
+	for (a = 0; s1[a] != '\0'; a++)
+		;
+
+	for (b = 0; s2[b] != '\0'; b++)
+		;
+
+	c = a + b + 1;
+
+	s3 = malloc(sizeof(char) * c);
+
+	if (s3 == NULL)
+		return (NULL);
+
+	for (a = 0; s1[a] != '\0'; a++)
+		s3[a] = s1[a];
+
+	for (b = 0; s2[b] != '\0'; b++)
+		s3[a + b] = s2[b];
+
+	return (s3);
+}
--- 0x0B-malloc_free/holberton.h
+++ 0x0B-malloc_free/holberton.h
@@ -1,5 +1,6 @@
 #ifndef _SAMPLE_HEADER_H_
 #define _SAMPLE_HEADER_H_
+char *str_concat(char *s1, char *s2);
 char *_strdup(char *str);
 char *create_array(unsigned int size, char c);
 int _putchar(char c);
