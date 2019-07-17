91fff9d108ae4389a89205a5c519c3c268814b5b
deibyocampo <deiby13_96@hotmail.com>
2019-07-17 21:14:55 +0000

    mountains ahead

---
+++ 0x0B-malloc_free/5-argstostr.c
@@ -0,0 +1,47 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * *argstostr - concatenates all arguments in the program.
+ *
+ * @ac: concantenates all arguments in the program.
+ *
+ * @av: a double pointer.
+ *
+ * Return: concatenates string followed by a line break.
+ */
+char *argstostr(int ac, char **av)
+{
+	int a, v, x, ls;
+	char *strn;
+
+	if (ac <= 0 || av == NULL)
+		return (NULL);
+
+	for (a = 0; a < ac; a++)
+	{
+		for (x = 0; av[a][x] != '\0'; x++)
+			ls++;
+	}
+
+	v = 0;
+
+	strn = malloc(sizeof(char) * (ls + a));
+
+	if (strn == NULL)
+	{
+		free(strn);
+		return (NULL);
+	}
+
+	for (a = 0; a < ac; a++)
+	{
+		for (x = 0; av[a][x] != '\0'; x++)
+		{
+			strn[v] = av[a][x];
+			v++;
+		}
+		strn[v] = '\n';
+		v++;
+	}
+	return (strn);
+}
