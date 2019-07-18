886c68acee5a618194f1d46460e12fc5aaddd7d5
deibyocampo <deiby13_96@hotmail.com>
2019-07-18 21:40:46 +0000

    string_nconcat

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,48 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * *string_nconcat - concatenates two strings.
+ *
+ * @s1: pointer of first string.
+ *
+ * @s2: pointer of second string.
+ *
+ * @n: size of integer.
+ *
+ * Return: concatenates string.
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int x;
+	unsigned int y;
+	unsigned int total;
+	char *conc;
+
+	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
+		s2 = "";
+
+	for (x = 0; s1[x] != '\0'; x++)
+		;
+
+	for (y = 0; s2[y] != '\0'; y++)
+		;
+
+	total = x + y;
+
+	conc = malloc(sizeof(char) * total);
+
+	if (conc == NULL)
+		return (NULL);
+
+	for (x = 0; s1[x] != '\0'; x++)
+		conc[x] = s1[x];
+
+	for (y = 0; y < n; y++)
+		conc[x + y] = s2[y];
+
+	return (conc);
+}
--- 0x0C-more_malloc_free/holberton.h
+++ 0x0C-more_malloc_free/holberton.h
@@ -1,4 +1,5 @@
 #ifndef _SAMPLE_HEADER_H_
 #define _SAMPLE_HEADER_H_
+char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *malloc_checked(unsigned int b);
 #endif /* _SAMPLE_HEADER_H_ */
