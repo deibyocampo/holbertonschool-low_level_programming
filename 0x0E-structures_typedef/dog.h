2c63b32801b51184054255b3fa857f70eda9736d
deibyocampo <deiby13_96@hotmail.com>
2019-07-22 23:41:40 +0000

    Django

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DJANGO
+#define DJANGO
+/**
+ * struct dog - Define the following elements.
+ *
+ * @name: Pointer string.
+ * @age: Float the age.
+ * @owner: Name of owner.
+ *
+ * Description: Longer description
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
