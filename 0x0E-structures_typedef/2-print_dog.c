50cd6c22d2a0e9c674f5241143c8a527a5c190bd
deibyocampo <deiby13_96@hotmail.com>
2019-07-23 00:30:21 +0000

    A dog will teach

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "dog.h"
+/**
+ * print_dog - print to information the dog
+ *
+ * @d: pointer pointer that runs through a data structure.
+ *
+ * Return: information of dog.
+ */
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		return;
+
+	if (d->name == NULL)
+		printf("Name: (nil)\n");
+	else
+		printf("Name: %s\n", d->name);
+
+	printf("Age: %f\n", d->age);
+
+	if (d->owner == NULL)
+		printf("Owner: (nill)\n");
+	else
+		printf("Owner: %s\n", d->owner);
+}
