9384e2183d27011e688654156c0ea2ef12a67a3b
deibyocampo <deiby13_96@hotmail.com>
2019-07-24 20:41:38 +0000

    What's my name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+#include <stdio.h>
+/**
+ * print_name - that prints a name.
+ *
+ * @name: pointer the string.
+ *
+ * @f: pointer.
+ *
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL)
+		f(name);
+}
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,4 @@
+#ifndef _SAMPLE_HEADER_H_
+#define _SAMPLE_HEADER_H_
+void print_name(char *name, void (*f)(char *));
+#endif
