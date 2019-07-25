476ef241480af0cf6bb31352309a69c9e17fcfee
deibyocampo <deiby13_96@hotmail.com>
2019-07-25 02:18:03 +0000

    something to aim at

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -23,7 +23,7 @@ int main(int argc, char *argv[])
 	}
 
 	if (!(argv[2][0] == '+' || argv[2][0] == '*' || argv[2][0] == '/' ||
-		argv[2][0] == '-' || argv[2][0] == '%') || argv[2][1] == '\0')
+		argv[2][0] == '-' || argv[2][0] == '%') || argv[2][1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
