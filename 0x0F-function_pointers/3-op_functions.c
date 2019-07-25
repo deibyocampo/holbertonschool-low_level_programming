a7ce7de7de3014d69acff03d7b8065cf5804aa9e
deibyocampo <deiby13_96@hotmail.com>
2019-07-25 02:01:39 +0000

    it often serves

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -9,7 +9,7 @@
  */
 int op_add(int a, int b)
 {
-	return(a + b);
+	return (a + b);
 }
 /**
  * op_sub - subtraction a and b
@@ -21,7 +21,7 @@ int op_add(int a, int b)
  */
 int op_sub(int a, int b)
 {
-	return(a - b);
+	return (a - b);
 }
 /**
  * op_mul - multiplication a and b.
@@ -33,7 +33,7 @@ int op_sub(int a, int b)
  */
 int op_mul(int a, int b)
 {
-	return(a * b);
+	return (a * b);
 }
 /**
  * op_div - division a and b.
@@ -45,7 +45,7 @@ int op_mul(int a, int b)
  */
 int op_div(int a, int b)
 {
-	return(a / b);
+	return (a / b);
 }
 /**
  * op_mod - module a and b.
@@ -57,5 +57,5 @@ int op_div(int a, int b)
  */
 int op_mod(int a, int b)
 {
-	return(a % b);
+	return (a % b);
 }
