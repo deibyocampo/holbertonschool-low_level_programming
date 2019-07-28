d3da0819d582af62cf837cbf9a1caa637d556629
deibyocampo <deiby13_96@hotmail.com>
2019-07-28 00:09:17 -0500

    string

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -23,7 +23,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 				printf("(nil)");
 			else
 				printf("%s", mm);
-			if (a < n - 1 && separator != NULL)
+			if ((a < n - 1) && separator != NULL)
 				printf("%s", separator);
 		}
 
