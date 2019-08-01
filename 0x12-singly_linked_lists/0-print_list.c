eab0e320e10d3f4d4c12601c37c5e94ef127a9be
deibyocampo <deiby13_96@hotmail.com>
2019-08-01 22:39:50 +0000

    number of nodes

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * print_list - ricevies to argument for print data.
+ *
+ * @h: pointer.
+ *
+ * Return: a number to nodes.
+ */
+size_t print_list(const list_t *h)
+{
+	size_t a;
+
+	for (a = 0; h != NULL; a++)
+	{
+		if (h->str == NULL)
+			printf("[0] (nill)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+
+		h = h->next;
+	}
+
+	return (a);
+}
