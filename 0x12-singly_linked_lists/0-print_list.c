e7f2fa8c644f8b1d523d25a4f1845553888793b9
deibyocampo <deiby13_96@hotmail.com>
2019-08-01 23:52:02 +0000

    print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -14,7 +14,7 @@ size_t print_list(const list_t *h)
 	for (a = 0; h != NULL; a++)
 	{
 		if (h->str == NULL)
-			printf("[0] (nil)\n");
+			printf("[0] %p\n", h->str);
 		else
 			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
