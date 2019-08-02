4689ab546f633fbe5636e7de00f494f216eda195
deibyocampo <deiby13_96@hotmail.com>
2019-08-02 00:14:38 +0000

    print list node

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,13 +11,15 @@ size_t print_list(const list_t *h)
 {
 	size_t a;
 
-	for (a = 0; h != NULL; a++)
+	a = 0;
+	while (h != NULL)
 	{
 		if (h->str == NULL)
 			printf("[0] %p\n", h->str);
 		else
 			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
+		a++;
 	}
 	return (a);
 }
