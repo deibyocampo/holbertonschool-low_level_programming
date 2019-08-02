d5ac2daabd2af68ef23452269938461641c01c41
deibyocampo <deiby13_96@hotmail.com>
2019-08-02 02:52:56 +0000

    free list

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,15 +1,15 @@
 #include "lists.h"
 /**
+ * free_list - free node the linked
  *
- *
- *
- *
+ * @head: pointer to list
  *
  */
 void free_list(list_t *head)
 {
 	list_t *p;
 	list_t *tmp;
+
 	p = head;
 
 	tmp = p->next;
