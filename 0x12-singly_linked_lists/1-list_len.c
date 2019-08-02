1ae73da45342230984e27806fabc210cae118662
deibyocampo <deiby13_96@hotmail.com>
2019-08-02 00:49:18 +0000

    print list node

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -12,8 +12,5 @@ size_t list_len(const list_t *h)
 	size_t a;
 
 	for (a = 0; h != NULL; a++)
-	{
-		printf("[%d]", h->len);
-	}
-	return(a);
+		return(a);
 }
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,5 @@
-#ifndef _link_
-#define _link_
+#ifndef LISTS_H
+#define LISTS_H
 #include <stddef.h>
 #include <stdio.h>
 #include <string.h>
@@ -15,10 +15,10 @@
  */
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
-#endif
+#endif /* LISTS_H */
