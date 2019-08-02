ba112b1085fc9593446abe763af06e27729cc1ab
deibyocampo <deiby13_96@hotmail.com>
2019-08-02 00:58:56 +0000

    list length

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,16 +1,19 @@
 #include "lists.h"
 
 /**
+ * list_len - print number of elements in a linked.
  *
+ * @h: pointer to the list
  *
- *
- *
- *
+ * Return: elements of linked
  */
 size_t list_len(const list_t *h)
 {
 	size_t a;
 
 	for (a = 0; h != NULL; a++)
-		return(a);
+	{
+		h = h->next;
+	}
+	return (a);
 }
