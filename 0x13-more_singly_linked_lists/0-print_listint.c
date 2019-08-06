0bf4f755e7cb38e45af5a0b035794a16f3d23aaf
deibyocampo <deiby13_96@hotmail.com>
2019-08-05 23:01:49 -0500

    print list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,11 +10,11 @@ size_t print_listint(const listint_t *h)
 {
 	size_t a;
 
-	while(h != NULL)
+	while (h != NULL)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
 		a++;
 	}
-	return(a);
+	return (a);
 }
