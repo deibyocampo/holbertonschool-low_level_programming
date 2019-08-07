b0eff1c003a55132f4c59dae5f18430d52e8f4be
deibyocampo <deiby13_96@hotmail.com>
2019-08-06 23:38:22 -0500

    print save version

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+/**
+ * print_listint_safe - print a list whith the location in memory of each n
+ *
+ * @head: the list
+ *
+ * Return: the number of node
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+	size_t size = 0;
+
+	while (head)
+	{
+		printf("[%p] %d\n", (void *) head, head->n);
+		size++;
+
+		if (head > head->next)
+			head = head->next;
+		else
+		{
+			head = head->next;
+			printf("->[%p] %d\n", (void *) head, head->n);
+			break;
+		}
+	}
+
+	return (size);
+}
