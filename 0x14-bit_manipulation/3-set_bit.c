fc9fcc44a9afd2521fcf89cbd092cbe937eb5705
deibyocampo <deiby13_96@hotmail.com>
2019-08-08 23:15:03 -0500

    set bit mask

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,12 +1,20 @@
 #include "holberton.h"
 /**
+ * set_bit - set the value of bit to one at a given index.
+ * @n: input values.
+ * @index: index.
  *
- *
- *
- *
- *
+ * Return: 1 if successful or -1 if there is an error.
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
+	unsigned long int proct = 1;
 
+	if (index > (sizeof(n) * 8))
+		return (-1);
+
+	proct = proct << index;
+
+	*n = (*n | proct);
+		return (1);
 }
