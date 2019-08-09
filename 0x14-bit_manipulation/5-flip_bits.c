3112287f9def6ab961095dbec9af7aee79ce30c7
deibyocampo <deiby13_96@hotmail.com>
2019-08-08 23:39:52 -0500

    flip bit

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,12 +1,25 @@
 #include "holberton.h"
 /**
+ * flip_bits - checks for the number of bits need to flip from n to m
+ * @n: the number to compare with m
+ * @m: the number to compare with n
  *
- *
- *
- *
- *
+ * Return: return the number of bits counted from flipping bits of n and m
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
+	unsigned long int proct = 1;
+	unsigned long int temp = 0;
 
+
+	temp = n ^ m;
+	proct = 0;
+
+	while (temp)
+	{
+		proct += temp & 1;
+		temp >>= 1;
+	}
+
+	return (proct);
 }
