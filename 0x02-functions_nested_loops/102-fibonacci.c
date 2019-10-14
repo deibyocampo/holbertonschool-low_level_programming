365013bd1fb0c0bbf474ae2f3c206652bd894885
deibyocampo <deiby13_96@hotmail.com>
2019-10-13 21:49:14 -0500

    fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+ * main - print fibonacci
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	unsigned long a, b, c;
+	int d;
+
+	a = 1;
+	b = 2;
+	d = 0;
+
+	printf("%lu, ", a);
+	while (d < 50)
+	{
+		printf("%lu", b);
+		if (d == 49)
+		{
+			printf("\n");
+		}
+		else
+		{
+			printf(", ");
+			c = a + b;
+			a = b;
+			b = c;
+		}
+		d++;
+	}
+	return (0);
+}
