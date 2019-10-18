0f0bafb94fb7c7ee01b702f60da01a178203a7cf
deibyocampo <deiby13_96@hotmail.com>
2019-10-18 14:57:59 -0500

    hash table deltete

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,34 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - deletes a hash table
+ * @ht: pointer to struct
+ * Return: void
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int i = 0;
+	hash_node_t *temp;
+
+	if (ht == NULL)
+		return;
+
+	while (i < ht->size)
+	{
+		temp = ht->array[i];
+		if (temp != NULL)
+		{
+			while (ht->array[i])
+			{
+				temp = ht->array[i];
+				free(temp->value);
+				free(temp->key);
+				ht->array[i] = ht->array[i]->next;
+				free(temp);
+			}
+		}
+		i++;
+	}
+	free(ht->array);
+	free(ht);
+}
