0e3f85a12a1fd3b1c349c3a21a5b2513aa39ff71
deibyocampo <deiby13_96@hotmail.com>
2019-10-29 23:12:06 -0500

    addin conditions

--- 0x1B-sorting_algorithms/0-bubble_sort.c
+++ 0x1B-sorting_algorithms/0-bubble_sort.c
@@ -9,6 +9,9 @@ void bubble_sort(int *array, size_t size)
 {
 	size_t i, j, k;
 
+	if (array == NULL || size == 0)
+		return;
+
 	for (i = 0; i < (size - 1); i++)
 	{
 		for (j = 0; j < (size - i - 1); j++)
