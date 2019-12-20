cea2cf314cee1736d22a4ec0dcb6716b8bd0d52e
deibyocampo <deiby13_96@hotmail.com>
2019-12-20 05:52:18 -0500

    binary tree is complete, rotate left and rotate right

---
+++ 0x1D-binary_trees/103-binary_tree_rotate_left.c
@@ -0,0 +1,26 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_rotate_left - performs a left-rotation on a binary tree
+ * @tree: pointer to the root node of the tree to rotate
+ * Return: return a pointer to the new root node of the tree once rotated
+ */
+binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
+{
+	binary_tree_t *node;
+
+	if (tree == NULL || tree->right == NULL)
+		return (tree);
+
+	node = tree->right;
+	node->parent = tree->parent;
+	tree->parent = node;
+	tree->right = node->left;
+
+	if (tree->right != NULL)
+		tree->right->parent = tree;
+
+	node->left = tree;
+	return (node);
+}
+
