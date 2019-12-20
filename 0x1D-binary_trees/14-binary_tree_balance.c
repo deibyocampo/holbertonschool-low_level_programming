0b2cfe450c29e75ed3e4f405560769e84758cf9b
deibyocampo <deiby13_96@hotmail.com>
2019-12-20 00:53:58 -0500

    binary tree nodes and tree balance

---
+++ 0x1D-binary_trees/14-binary_tree_balance.c
@@ -0,0 +1,35 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_height - measures the height of a binary tree
+ * @tree: the input root address
+ * Return: height of the binary tree
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t rs, ls;
+
+	if (tree == NULL)
+		return (0);
+	ls = binary_tree_height(tree->left);
+	rs = binary_tree_height(tree->right);
+	if (ls > rs)
+		return (ls + 1);
+	return (rs + 1);
+}
+
+/**
+ * binary_tree_balance - measures the balance factor of a binary tree
+ * @tree: the input root address
+ * Return: balance of the binary tree
+ */
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	int rl, lg;
+
+	if (tree == NULL)
+		return (0);
+	lg = binary_tree_height(tree->left);
+	rl = binary_tree_height(tree->right);
+	return (lg - rl);
+}
