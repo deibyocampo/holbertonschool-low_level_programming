e8a7f1c009fdb905bbb773fb859707935cb992d6
deibyocampo <deiby13_96@hotmail.com>
2020-01-19 19:17:39 -0500

    change the print a linear search

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -16,7 +16,7 @@ int linear_search(int *array, size_t size, int value)
 
 	while (i < size)
 	{
-		printf("value checked array[%d] = [%d]\n", (int)i, array[i]);
+		printf("Value checked array[%d] = [%d]\n", (int)i, array[i]);
 		if (array[i] == value)
 			return (i);
 		i++;
